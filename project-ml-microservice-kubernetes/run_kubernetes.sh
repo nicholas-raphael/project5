f67d1a77d8472fcea9b377ec44ae6d62c98cd273
Nico <nicholas.farias@wizeline.com>
2022-05-15 14:59:09 -0500

    add changes for project

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,13 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=techdemowz/project5
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl create deploy project5 --image=techdemowz/project5
 # Step 3:
 # List kubernetes pods
-
+kubectl get deploy,rs,svc,pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/project5-796955cd89-p4wj7 --address 0.0.0.0 80:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=techdemowz/project5
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker tag project5 $dockerpath
+docker login
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
